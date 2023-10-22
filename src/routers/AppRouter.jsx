06306707dfc4b28e29e1235b32cad1d0d0f60809
Luis carlos <luisxhl7@gmail.com>
2023-10-21 22:41:00 -0500

    fix redirect in login

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Login } from '../components/pages/login/Login'
 import { PrivateRoutes } from './PrivateRoutes'
+import { useSelector } from 'react-redux'
 
 export const AppRouter = () => {
+  const {token} = useSelector((state) => state.token);
   
   return (
     <Routes>
